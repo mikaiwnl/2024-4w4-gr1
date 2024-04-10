fe23b7a4f89d897fff193bd6b39e656486bc3a33
mikaiwnll <you@example.com>
2024-04-10 10:47:10 -0400

    ajout de theme support

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 <?php 
     add_theme_support('menus'); 
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
     function register_my_menus() {
         register_nav_menus(
           array(
