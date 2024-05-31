efdc130644329d7f8b4cdff0631651c0401405ee
michael ali soeu <takumisoeu@gmail.com>
2024-05-31 15:22:02 -0400

    modification css

--- gabarits/template-pays.php
+++ gabarits/template-pays.php
@@ -6,20 +6,35 @@
 
 <?php get_header(); ?>
 
-<main class="pagePays">
-    
-<?php
-
-if ( have_posts() ) : the_post(); ?>
+<div class="mainPage" class="global">
 <h1><?= get_the_title(); ?></h1>
+<?php
+if ( have_posts() ) : the_post(); ?>
+</div>
+
+<div class="contenuPage">
 <?php the_content();?>
-<p>les meilleur destination</p>
-</main><!-- #main -->
-<div>
-<?php endif;?>
+
+
+<!-- Gallery -->
+<div class="galerie">
+   
+</div>
 
 <?php get_template_part('gabarits/vague') ?>
 
+<!-- REST API Section -->
+<div class="rest-api">
+    <select id="country-select">
+        <!-- Options will be populated with JavaScript -->
+    </select>
+    <div id="destinations">
+        <!-- Destinations will be populated with JavaScript -->
+    </div>
 </div>
-<?php
-get_footer();
+
+<?php get_template_part('gabarits/vague') ?>
+
+<?php endif;?>
+</div>
+<?php get_footer(); ?>
